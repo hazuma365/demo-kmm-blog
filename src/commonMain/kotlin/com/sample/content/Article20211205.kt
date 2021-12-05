ad40d68d403224a8db9788e3c89aaafe7de8de9b
hazuma365 <hazuma365@gmail.com>
2021-12-05 22:12:58 +0900

    test

---
+++ src/commonMain/kotlin/com/sample/content/Article20211205.kt
@@ -0,0 +1,17 @@
+package com.sample.content
+
+import com.sample.data.Article
+
+val article20200105 =
+    Article(
+        title = "2021-12-05",
+        categories = listOf("blog"),
+        date = "2021-12-05",
+        body = """
+
+# レスポンシブデザインにはまる
+WEBサイトベースでつくっていたため、SPで見るとずれることが判明。
+適当にDIVなどを組み合わせてきたが、大幅な工事が必要そう。
+
+    """.trimIndent()
+    )
--- src/commonMain/kotlin/com/sample/content/ArticleList.kt
+++ src/commonMain/kotlin/com/sample/content/ArticleList.kt
@@ -3,4 +3,5 @@ package com.sample.content
 val articleList = listOf(
     article20211121,
     article20200101,
+    article20200105,
 )
