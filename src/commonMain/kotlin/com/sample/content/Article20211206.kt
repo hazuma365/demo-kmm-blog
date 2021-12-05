41068cd50364a8ebb3d4a03d94198c12c991827e
hazuma365 <hazuma365@gmail.com>
2021-12-06 00:40:56 +0900

    test

---
+++ src/commonMain/kotlin/com/sample/content/Article20211206.kt
@@ -0,0 +1,38 @@
+package com.sample.content
+
+import com.sample.data.Article
+
+val article20200106 =
+    Article(
+        title = "2021-12-06",
+        categories = listOf("blog","aws"),
+        date = "2021-12-06",
+        body = """
+
+# AWS Amplifyを使う
+Java
+- https://docs.aws.amazon.com/ja_jp/corretto/latest/corretto-11-ug/generic-linux-install.html
+```yml
+version: 1
+frontend:
+  phases:
+    # IMPORTANT - Please verify your build commands
+    preBuild:
+      commands:
+      - rpm --import https://yum.corretto.aws/corretto.key
+      - curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+      - yum install -y java-11-amazon-corretto
+    build:
+      commands:
+      - ./gradlew jsBrowserProductionWebpack
+      - ls
+  artifacts:
+    # IMPORTANT - Please verify your build output directory
+    files:
+      - '**/*'
+    baseDirectory: /
+  cache:
+    paths: []
+```
+    """.trimIndent()
+    )
